7ff95cb1268b6e1cb5ae4008050e98c5b0c5ed7f
Ranjith01061994 <hirk01061994@gmail.com>
2021-04-28 10:44:17 +0530

    my class work done

--- src/org/comm/Day2.java
+++ src/org/comm/Day2.java
@@ -7,6 +7,8 @@ public class Day2 {
 		
 		System.out.println(1);
 		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
 		
 		try
 		{
---
+++ src/org/comm/Day4.java
@@ -0,0 +1,9 @@
+package org.comm;
+
+public class Day4 {
+	private void sysout() {
+System.out.println(1);
+System.out.println(2);
+	}
+
+}
